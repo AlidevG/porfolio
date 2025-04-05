6cc91907a328a9cb878bb3f2f43f3f4eba7d6900
Ali <Ali@gmail.com>
2025-04-05 15:26:45 +0500

    My Portifolio

---
+++ js/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    title: "Akademiknashr",
+    github: "https://github.com/AlidevG/Akademik.Nashr",
+    vercel: "https://akademik-nashr-gamma.vercel.app/",
+  },
+  {
+    title: "Cofee stret",
+    github: "https://github.com/AlidevG/Cofee-Figma",
+    vercel: "https://cofee-figma.vercel.app/",
+  },
+  {
+    title: "5Vazifa-1",
+    github: "https://github.com/AlidevG/5Vazifa-1",
+    vercel: "https://5-vazifa-1.vercel.app/",
+  },
+  {
+    title: "5Vazifa-4",
+    github: "https://github.com/AlidevG/5Vazifa-4",
+    vercel: "https://5-vazifa-4.vercel.app/",
+  }
+ 
+];
